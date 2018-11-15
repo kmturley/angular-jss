7bc1379296f35c35ddb41264a83932e5d4ede8e0
Kim T <kimturley@gmail.com>
2018-11-15 18:27:04 +1100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import jss from 'jss';
+import preset from 'jss-preset-default';
+
+jss.setup(preset());
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
